1768d92bd1ef1d8c72f777f64ea90d1447bf169b
jarquisol <jsandoval@codevschool.org>
2020-02-28 04:28:57 +0000

    Ya se muestra de manera adecuada la secuencia del monto ahorcándose

--- main.py
+++ main.py
@@ -1,26 +1,6 @@
 import random
 
-ganar = "Felicidades has ganado la palabra fue: \"{}\", tuvistes {} fallos"
-acertar = "Bien la letra: \"{}\" si se encontro"
-fallar = "Sigue intentandolo"
-intentos = 0
-palabras = ["zapato", "mocos", "otra","juanito", "ferrocarril", "parangaracutirimicuaro", "otorrinolaringologo", "langosta", "celular", "tijeras", "zancudo"]
-letrasAtinadas = []
-letraEvaluar = ""
-letraPropuesta = ""
-
-
-rand = random.randint(1,len(palabras))-1
-letrasFaltantes = len(palabras[rand])
-print("Bienvenido este es un juego del ahorcado, usted debe ingresar las letras para intentar adivinar la palabra que se genera de forma aleatoria")
-print() #este es un espacio en blanco
-print("No te preocupes, tienes todos los intentos que gustes")
-print("Cuando estes listo presina ENTER para iniciar....")
-input() 
-
-print("Iniciamos, la primera pista es que es una palabra de " + str(letrasFaltantes) + " letras. Representada por los asteriscos a continuación: ")
-
-print (
+monito1 = (
  "   +---+ \n"
  "   |   |\n"
  "       |\n"  
@@ -28,6 +8,82 @@ print (
  "       |\n"
  "       |\n"
  " =========\n")
+
+monito2 = (
+ "   +---+ \n"
+ "   |   |\n"
+ "   O   |\n"  
+ "       |\n"
+ "       |\n"
+ "       |\n"
+ " =========\n")
+
+monito3 = (
+ "   +---+ \n"
+ "   |   |\n"
+ "   O   |\n"  
+ "   |   |\n"
+ "       |\n"
+ "       |\n"
+ " =========\n")
+
+monito4 = (
+ "   +---+ \n"
+ "   |   |\n"
+ "   O   |\n"  
+ "   |\  |\n"
+ "       |\n"
+ "       |\n"
+ " =========\n")
+
+monito5 = (
+ "   +---+ \n"
+ "   |   |\n"
+ "   O   |\n"  
+ "  /|\  |\n"
+ "       |\n"
+ "       |\n"
+ " =========\n")
+
+monito6 = (
+ "   +---+ \n"
+ "   |   |\n"
+ "   O   |\n"  
+ "  /|\  |\n"
+ "    \  |\n"
+ "       |\n"
+ " =========\n")
+
+monito7 = (
+ "   +---+ \n"
+ "   |   |\n"
+ "   O   |\n"  
+ "  /|\  |\n"
+ "  / \  |\n"
+ "       |\n"
+ " =========\n")
+
+monitos = [monito1, monito2, monito3, monito4, monito5, monito6, monito7]
+
+ganar = "Felicidades has ganado! La palabra fue: \"{}\", tuvistes {} fallos"
+acertar = "Bien la letra: \"{}\" sí se encontró"
+fallar = "No está esa letra, sigue intentando"
+fallos = 0
+palabras = ["zapato", "mocos", "otra","juanito", "ferrocarril", "parangaracutirimicuaro", "otorrinolaringologo", "langosta", "celular", "tijeras", "zancudo", "computadora", "monitor", "lapiz", "cuaderno", "mesa", "arquitecto", "elefante", "tornamesa", "cuadrilatero", "antena", "triangulo"]
+letrasAtinadas = []
+letraEvaluar = ""
+letraPropuesta = ""
+
+rand = random.randint(1,len(palabras))-1
+letrasFaltantes = len(palabras[rand])
+print("Bienvenido, este es el juego del ahorcado. Debes ingresar las letras para intentar adivinar la palabra que se genera de forma aleatoria")
+print() #este es un espacio en blanco
+print("Tienes un número limitado de fallos, hasta que el mono se ahorque")
+print("Cuando estes listo presiona ENTER....")
+input() 
+print("Iniciamos, la primera pista es que es una palabra de " + str(letrasFaltantes) + " letras, representada por los siguientes espacios en blanco: ")
+print()
+print(monito1)
 
 while(letrasFaltantes != 0):
   letrasAcertadas = 0
@@ -38,17 +94,21 @@ while(letrasFaltantes != 0):
       print(letraEvaluar, end = " ")
       letrasAcertadas = letrasAcertadas + 1
     else:
-      print("*", end = " ")
+      print("_", end = " ")
   print()
   print()   
   if(letrasFaltantes == letrasAcertadas):    
-    print(ganar.format(palabras[rand], intentos))
+    print(ganar.format(palabras[rand], fallos))
     break
-  letraPropuesta = input("¿Ingrese una letra para evaluar? ")
+# De aquí hacia atrás son preparativos (setup). A partir de aquí no avanza el programa hasta que se ingrese un a letra por parte del usuario.
+  letraPropuesta = input("Ingrese una letra para evaluar ")
   if(letraPropuesta in palabras[rand]):
     letrasAtinadas.append(letraPropuesta)
     print(acertar.format(letraPropuesta))
   else: 
     print(fallar)
-    intentos = intentos + 1
-
+    fallos = fallos + 1
+    print(monitos[fallos])
+  if (fallos == 6):
+    print("Fuiste ahorcado!")
+    break
